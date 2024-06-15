e56978bb8689975469110f2acf5935b442ea0092
psawhney <psawhney@vmware.com>
2024-06-14 23:13:29 -0700

    Initial commit

---
+++ src/test/java/com/stride/data_ingestion_service/DataIngestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stride.data_ingestion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataIngestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
