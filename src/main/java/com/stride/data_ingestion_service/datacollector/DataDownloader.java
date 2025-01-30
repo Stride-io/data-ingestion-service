a1459777e9532d8a5ae6ae4dc2efdfc233a44f5c
psawhney <psawhney@vmware.com>
2025-01-29 18:14:49 -0800

    recover changes

---
+++ src/main/java/com/stride/data_ingestion_service/datacollector/DataDownloader.java
@@ -0,0 +1,37 @@
+package com.stride.data_ingestion_service.datacollector;
+
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Component
+public class DataDownloader {
+
+    private static final long FIXED_RATE = 12 * 60 * 60 * 1000; // Runs every 12 hours
+
+
+    public void runScript() {
+        try {
+            // Replace with the actual path to your Python script
+            String[] command = {"python", "/path/to/your/script.py"};
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+
+            Process process = processBuilder.start();
+
+            // Read output from the Python script
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("Process exited with code: " + exitCode);
+
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/script/downloader.py
@@ -0,0 +1,46 @@
+import os
+import requests
+import kaggle
+import random
+
+
+os.environ['KAGGLE_USERNAME'] = "" #toDo - get from deployment
+os.environ['KAGGLE_KEY'] = "" #toDo - get from deployment
+
+
+def get_priority(search_result):
+    metadata = vars(search_result)
+    if metadata['usabilityRating'] < 0.5:
+        return 0
+    return metadata['usabilityRating']+ (100*metadata['voteCount']) + (10*metadata['downloadCount'])
+
+def get_random_words(num_words):
+    api_url = "https://random-word-api.herokuapp.com/word"
+    params = {
+        "number": num_words,
+        "length": random.randint(1, 10)
+    }
+
+    response = requests.get(api_url, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+if __name__ == "__main__":
+    random_words = get_random_words(10)
+    while len(random_words) == 0:
+        random_words = get_random_words(10)
+    for word in random_words:
+        print("word = ", word)
+        search_results = kaggle.api.dataset_list(search = word)
+        print("search_results = ", search_results)
+        if len(search_results) == 0:
+            continue
+        sorted_results = sorted(search_results, key=get_priority)
+        if len(sorted_results):
+            kaggle.api.dataset_download_files(
+                dataset = sorted_results[0].ref,
+                path='/Users/pinaksawhney/Desktop/Project/kaggle_test_datat',
+                unzip=True
+            )
+            break
